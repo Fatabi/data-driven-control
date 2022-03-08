faac6c4f1616c6b71c123cf73c4b7f1f4264f9be
Deniz Etkar <denizetkar@gmail.com>
2022-03-08 21:52:46 +0100

    CartPole: ported the vanilla PyTorch to pytorch lightning

--- cart_pole.py
+++ cart_pole.py
@@ -1,14 +1,14 @@
-import time
 from math import pi
+from typing import List, Optional
 
 import matplotlib.pyplot as plt
 import pandas as pd
-import torch
+import pytorch_lightning as pl
+import torch as th
 import torch.nn as nn
-
-# from torchdyn.core import NeuralODE
-# from torchdyn.numerics import Euler, HyperEuler, odeint
-# from torchdyn.datasets import *
+from torch.optim import Adam
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from torch.utils.data import DataLoader, Dataset
 from torchdyn.numerics import odeint
 
 
@@ -27,60 +27,63 @@ class ControlledCartPole(nn.Module):
         M = 0.5
         m = 0.2
         b = 0.1
-        I = 0.006
+        inertia = 0.006
         g = 9.8
-        l = 0.3
+        length = 0.3
 
-        p = I * (M + m) + M * m * l**2  # denominator for the A and B matrices
+        p = inertia * (M + m) + M * m * length**2  # denominator for the A and B matrices
 
-        self.A = torch.tensor(
-            [
-                [0, 1, 0, 0],
-                [0, -(I + m * l**2) * b / p, (m**2 * g * l**2) / p, 0],
-                [0, 0, 0, 1],
-                [0, -(m * l * b) / p, m * g * l * (M + m) / p, 0],
-            ]
+        self.register_buffer(
+            "A",
+            th.tensor(
+                [
+                    [0, 1, 0, 0],
+                    [0, -(inertia + m * length**2) * b / p, (m**2 * g * length**2) / p, 0],
+                    [0, 0, 0, 1],
+                    [0, -(m * length * b) / p, m * g * length * (M + m) / p, 0],
+                ]
+            ),
         )
-        self.B = torch.tensor([[0, (I + m * l**2) / p, 0, m * l / p]])
+        self.A: th.Tensor
+        self.register_buffer("B", th.tensor([[0, (inertia + m * length**2) / p, 0, m * length / p]]))
+        self.B: th.Tensor
 
     def forward(self, t, x):
         self.nfe += 1
         self.cur_u = self.u(t, x)
-        self.cur_f = torch.matmul(x, self.A.T).T.permute([1, 0]) + torch.matmul(self.cur_u, self.B)
+        self.cur_f = th.matmul(x, self.A.T).T.permute([1, 0]) + th.matmul(self.cur_u, self.B)
         return self.cur_f
 
 
 class IntegralCost(nn.Module):
     """Integral cost function
     Args:
-        x_star: torch.tensor, target position
+        x_star: th.tensor, target position
         P: float, terminal cost weights
         Q: float, state weights
         R: float, controller regulator weights
     """
 
-    def __init__(self, x_star, P=0, Q=1, R=0):
+    def __init__(self, x_star: th.Tensor, P: float = 0.0, Q: float = 1.0, R: float = 0.0):
         super().__init__()
-        self.x_star = x_star
+        self.register_buffer("x_star", x_star)
+        self.x_star: th.Tensor
         self.P, self.Q, self.R, = (
             P,
             Q,
             R,
         )
 
-    def forward(self, x, u=torch.Tensor([0.0])):
+    def forward(self, x: th.Tensor, u: Optional[th.Tensor] = None):
         """
         x: trajectory
         u: control input
         """
-        cost = self.P * torch.norm(x[-1] - self.x_star, p=2, dim=-1).mean()
-        cost += self.Q * torch.norm(x - self.x_star, p=2, dim=-1).mean()
-        cost += self.R * torch.norm(u - 0, p=2).mean()
+        cost = self.P * th.norm(x[-1] - self.x_star, p=2, dim=-1).mean()
+        cost += self.Q * th.norm(x - self.x_star, p=2, dim=-1).mean()
+        if u is not None:
+            cost += self.R * th.norm(u - 0, p=2).mean()
         return cost
-
-
-device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
-# The controller is a simple MLP with one hidden layer with bounded output
 
 
 class NeuralController(nn.Module):
@@ -91,73 +94,100 @@ class NeuralController(nn.Module):
 
     def forward(self, t, x):
         x = self.model(x)
-        return torch.clamp(x, self.u_min, self.u_max)
+        return th.clamp(x, self.u_min, self.u_max)
 
 
-model = nn.Sequential(nn.Linear(4, 32), nn.Tanh(), nn.Linear(32, 1)).to(device)
-u = NeuralController(model)
-for p in u.model[-1].parameters():
-    torch.nn.init.zeros_(p)
+class CartPoleTrainer(pl.LightningModule):
+    def __init__(self, sys: ControlledCartPole, x_star: th.Tensor, t_span: th.Tensor, max_epochs: int, lr: float = 1e-3):
+        super().__init__()
+        self.sys = sys
+        self.x_star = x_star
+        self.t_span = t_span
+        self.max_epochs = max_epochs
+        self.lr = lr
+        self.cost_func = IntegralCost(x_star)
 
-# Controlled system
-sys = ControlledCartPole(u).to(device)
+    def configure_optimizers(self):
+        optim = Adam(self.parameters(), lr=self.lr)
+        sched = CosineAnnealingLR(optim, T_max=self.max_epochs, eta_min=1e-8)
+        return [optim], [sched]
+
+    def forward(self, x0: th.Tensor, t_span: Optional[th.Tensor] = None) -> th.Tensor:
+        if t_span is None:
+            t_span = self.t_span
+        _, trajectory = odeint(self.sys, x0, t_span, solver="dopri5", atol=1e-6, rtol=1e-6)
+        return trajectory
+
+    def training_step(self, batch: th.Tensor, batch_idx: int) -> th.Tensor:
+        x0 = batch
+        _, trajectory = odeint(self.sys, x0, self.t_span, solver="dopri5", atol=1e-6, rtol=1e-6)
+        loss = self.cost_func(trajectory)
+
+        self.log("train_loss", loss)
+        return loss
 
 
-# Loss function declaration
-x_star = torch.Tensor([0.0, 0.0, pi, 0.0]).to(device)
-cost_func = IntegralCost(x_star)
+class TrajDataset(Dataset):
+    def __init__(self, lb: List[float], ub: List[float], batch_size: int):
+        super().__init__()
+        self.lb = lb
+        self.ub = ub
+        self.batch_size = batch_size
+        self.init_dist = th.distributions.Uniform(th.Tensor(lb), th.Tensor(ub))
 
-# Time span
-t0, tf = 0, 2  # initial and final time for controlling the system
-steps = 20 + 1  # so we have a time step of 0.1s
-t_span = torch.linspace(t0, tf, steps).to(device)
+    def __len__(self) -> int:
+        return self.batch_size * 5
 
-# Initial distribution
-min_bound = [-1, -2, -pi, -pi * 2]
-max_bound = [1, 2, pi, pi * 2]
+    def __getitem__(self, _: int) -> th.Tensor:
+        return self.init_dist.sample((1,)).flatten()
 
-init_dist = torch.distributions.Uniform(torch.Tensor(min_bound), torch.Tensor(max_bound))
-# Hyperparameters
-lr = 3e-3
-epochs = 500
-bs = 1024
-opt = torch.optim.Adam(u.parameters(), lr=lr)
 
-# Training loop
-t0 = time.time()
-losses = []
-for e in range(epochs):
-    x0 = init_dist.sample((bs,)).to(device)
-    _, trajectory = odeint(sys, x0, t_span, solver="dopri5", atol=1e-5, rtol=1e-5)
-    loss = cost_func(trajectory)
-    losses.append(loss.detach().cpu().item())
-    loss.backward()
-    opt.step()
-    opt.zero_grad()
-    print("Loss {:.4f} , epoch {}".format(loss.item(), e), end="\r")
-timing = time.time() - t0
-print("\nTraining time: {:.4f} s".format(timing))
+if __name__ == "__main__":
+    control_model = nn.Sequential(nn.Linear(4, 32), nn.Tanh(), nn.Linear(32, 1))
+    u = NeuralController(control_model)
+    # Controlled system
+    sys = ControlledCartPole(u)
+    # Loss function declaration
+    x_star = th.Tensor([0.0, 0.0, pi, 0.0])
 
-# Testing the controller on the real system
-x0 = init_dist.sample((100,)).to(device)
-t0, tf, steps = 0, 2, 20 * 10 + 1
-t_span_fine = torch.linspace(t0, tf, steps).to(device)
-_, traj = odeint(sys, x0, t_span_fine, solver="dopri5", atol=1e-5, rtol=1e-5)
-t_span_fine = t_span_fine.detach().cpu()
-traj = traj.detach().cpu()
+    # Time span
+    t0, tf = 0, 2  # initial and final time for controlling the system
+    steps = 20 + 1  # so we have a time step of 0.1s
+    t_span = th.linspace(t0, tf, steps)
+    # Hyperparameters
+    lr = 1e-3
+    max_epochs = 300
+    batch_size = 1024
+    # Initial distribution
+    lb = [-1, -2, -pi, -pi * 2]
+    ub = [1, 2, pi, pi * 2]
 
-# Plotting
-fig, ax = plt.subplots(1, 1, figsize=(12, 6))
-for i in range(len(x0)):
-    ax.plot(t_span_fine, traj[:, i, 0], "k", alpha=0.3, label="x" if i == 0 else None)
-    ax.plot(t_span_fine, traj[:, i, 1], "b", alpha=0.3, label="x_dot" if i == 0 else None)
-    ax.plot(t_span_fine, traj[:, i, 2], "r", alpha=0.3, label="theta" if i == 0 else None)
-    ax.plot(t_span_fine, traj[:, i, 3], "g", alpha=0.3, label="theta_dot" if i == 0 else None)
-ax.legend()
-ax.set_title("Controlled trajectories")
-ax.set_xlabel(r"$t~[s]$")
+    model = CartPoleTrainer(sys, x_star, t_span, max_epochs, lr)
+    trainer = pl.Trainer(gpus=[0], max_epochs=max_epochs, log_every_n_steps=5)
+    dataset = TrajDataset(lb, ub, batch_size)
+    dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=1, persistent_workers=True)
+    trainer.fit(model, dataloader)
 
-# Exporting Datas
-traj_0 = torch.cat([t_span_fine.unsqueeze(-1), traj[:, 0]], dim=-1)
-df = pd.DataFrame(traj_0)
-df.to_csv("./veriler.csv", index=False)
+    # Testing the controller on the real system
+    x0 = dataset.init_dist.sample((100,)).to(model.device)
+    t0, tf, steps = 0, 2, 20 * 10 + 1
+    t_span_fine = th.linspace(t0, tf, steps).to(model.device)
+    traj = model.forward(x0, t_span=t_span_fine)
+    t_span_fine = t_span_fine.detach().cpu()
+    traj = traj.detach().cpu()
+
+    # Plotting
+    fig, ax = plt.subplots(1, 1, figsize=(12, 6))
+    for i in range(len(x0)):
+        ax.plot(t_span_fine, traj[:, i, 0], "k", alpha=0.3, label="x" if i == 0 else None)
+        ax.plot(t_span_fine, traj[:, i, 1], "b", alpha=0.3, label="x_dot" if i == 0 else None)
+        ax.plot(t_span_fine, traj[:, i, 2], "r", alpha=0.3, label="theta" if i == 0 else None)
+        ax.plot(t_span_fine, traj[:, i, 3], "g", alpha=0.3, label="theta_dot" if i == 0 else None)
+    ax.legend()
+    ax.set_title("Controlled trajectories")
+    ax.set_xlabel(r"$t~[s]$")
+
+    # Exporting Datas
+    traj_0 = th.cat([t_span_fine.unsqueeze(-1), traj[:, 0]], dim=-1)
+    df = pd.DataFrame(traj_0)
+    df.to_csv("./veriler.csv", index=False)
