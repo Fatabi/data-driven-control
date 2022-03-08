a631648e9dce2a790fb173e07b4be0f9d07851b9
Deniz Etkar <denizetkar@gmail.com>
2022-03-08 23:42:16 +0100

    added ZeroController that always outputs a scalar 0 control signal; added system dynamics regularization IntegralWReg for smoother trajectories; fixed a bunch of device placement related bugs; using the more high-level interface ODEProblem instead of odeint -> allows to also use the adjoint method

--- cart_pole.py
+++ cart_pole.py
@@ -1,5 +1,5 @@
 from math import pi
-from typing import List, Optional
+from typing import Callable, List, Optional
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -9,7 +9,9 @@ import torch.nn as nn
 from torch.optim import Adam
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from torch.utils.data import DataLoader, Dataset
-from torchdyn.numerics import odeint
+from torchdyn.core import ODEProblem
+
+th.set_default_dtype(th.float64)
 
 
 class ControlledCartPole(nn.Module):
@@ -17,7 +19,7 @@ class ControlledCartPole(nn.Module):
     Inverted pendulum with torsional spring
     """
 
-    def __init__(self, u):
+    def __init__(self, u: Callable[[th.Tensor, th.Tensor], th.Tensor]):
         super().__init__()
         self.u = u  # controller (nn.Module)
         self.nfe = 0  # number of function evaluations
@@ -48,7 +50,7 @@ class ControlledCartPole(nn.Module):
         self.register_buffer("B", th.tensor([[0, (inertia + m * length**2) / p, 0, m * length / p]]))
         self.B: th.Tensor
 
-    def forward(self, t, x):
+    def forward(self, t: th.Tensor, x: th.Tensor) -> th.Tensor:
         self.nfe += 1
         self.cur_u = self.u(t, x)
         self.cur_f = th.matmul(x, self.A.T).T.permute([1, 0]) + th.matmul(self.cur_u, self.B)
@@ -87,25 +89,47 @@ class IntegralCost(nn.Module):
 
 
 class NeuralController(nn.Module):
-    def __init__(self, model, u_min=-20, u_max=20):
+    def __init__(self, model: nn.Module, gain: float = 40.0):
         super().__init__()
         self.model = model
-        self.u_min, self.u_max = u_min, u_max
+        self.gain = gain
+        self.normalizer = nn.Softsign()
 
-    def forward(self, t, x):
-        x = self.model(x)
-        return th.clamp(x, self.u_min, self.u_max)
+    def forward(self, t: th.Tensor, x: th.Tensor) -> th.Tensor:
+        return self.normalizer(self.model(x)) * self.gain
+
+
+class ZeroController(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, t: th.Tensor, x: th.Tensor) -> th.Tensor:
+        return th.zeros_like(t)
+
+
+class IntegralWReg(nn.Module):
+    def __init__(self, sys: Callable[[th.Tensor, th.Tensor], th.Tensor], reg_coef: float = 1e-4):
+        super().__init__()
+        self.sys = sys
+        self.reg_coef = reg_coef
+
+    def forward(self, t: th.Tensor, x: th.Tensor) -> th.Tensor:
+        loss = self.reg_coef * th.abs(self.sys(x)).sum(1)
+        return loss
 
 
 class CartPoleTrainer(pl.LightningModule):
     def __init__(self, sys: ControlledCartPole, x_star: th.Tensor, t_span: th.Tensor, max_epochs: int, lr: float = 1e-3):
         super().__init__()
         self.sys = sys
-        self.x_star = x_star
-        self.t_span = t_span
+        self.register_buffer("x_star", x_star)
+        self.x_star: th.Tensor
+        self.register_buffer("t_span", t_span)
+        self.t_span: th.Tensor
         self.max_epochs = max_epochs
         self.lr = lr
         self.cost_func = IntegralCost(x_star)
+        self.ode_problem = ODEProblem(self.sys, solver="dopri5", sensitivity="autograd", integral_loss=IntegralWReg(self.sys))
 
     def configure_optimizers(self):
         optim = Adam(self.parameters(), lr=self.lr)
@@ -115,12 +139,12 @@ class CartPoleTrainer(pl.LightningModule):
     def forward(self, x0: th.Tensor, t_span: Optional[th.Tensor] = None) -> th.Tensor:
         if t_span is None:
             t_span = self.t_span
-        _, trajectory = odeint(self.sys, x0, t_span, solver="dopri5", atol=1e-6, rtol=1e-6)
+        _, trajectory = self.ode_problem.odeint(x0, t_span)
         return trajectory
 
     def training_step(self, batch: th.Tensor, batch_idx: int) -> th.Tensor:
         x0 = batch
-        _, trajectory = odeint(self.sys, x0, self.t_span, solver="dopri5", atol=1e-6, rtol=1e-6)
+        _, trajectory = self.ode_problem.odeint(x0, t_span)
         loss = self.cost_func(trajectory)
 
         self.log("train_loss", loss)
@@ -143,7 +167,7 @@ class TrajDataset(Dataset):
 
 
 if __name__ == "__main__":
-    control_model = nn.Sequential(nn.Linear(4, 32), nn.Tanh(), nn.Linear(32, 1))
+    control_model = nn.Sequential(nn.Linear(4, 128), nn.PReLU(), nn.Linear(128, 1))
     u = NeuralController(control_model)
     # Controlled system
     sys = ControlledCartPole(u)
@@ -152,12 +176,12 @@ if __name__ == "__main__":
 
     # Time span
     t0, tf = 0, 2  # initial and final time for controlling the system
-    steps = 20 + 1  # so we have a time step of 0.1s
+    steps = 10 * tf + 1  # so we have a time step of 0.1s
     t_span = th.linspace(t0, tf, steps)
     # Hyperparameters
-    lr = 1e-3
-    max_epochs = 300
-    batch_size = 1024
+    lr = 5e-3
+    max_epochs = 600
+    batch_size = 512
     # Initial distribution
     lb = [-1, -2, -pi, -pi * 2]
     ub = [1, 2, pi, pi * 2]
@@ -170,7 +194,8 @@ if __name__ == "__main__":
 
     # Testing the controller on the real system
     x0 = dataset.init_dist.sample((100,)).to(model.device)
-    t0, tf, steps = 0, 2, 20 * 10 + 1
+    t0, tf = 0, 10  # initial and final time for controlling the system
+    steps = 10 * tf + 1  # so we have a time step of 0.1s
     t_span_fine = th.linspace(t0, tf, steps).to(model.device)
     traj = model.forward(x0, t_span=t_span_fine)
     t_span_fine = t_span_fine.detach().cpu()
@@ -190,4 +215,4 @@ if __name__ == "__main__":
     # Exporting Datas
     traj_0 = th.cat([t_span_fine.unsqueeze(-1), traj[:, 0]], dim=-1)
     df = pd.DataFrame(traj_0)
-    df.to_csv("./veriler.csv", index=False)
+    df.to_csv("./trajectory_0.csv", index=False)
